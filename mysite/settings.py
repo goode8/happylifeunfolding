9ee882f545bd683a992ed374eb35d6653afc0db0
goode8 <happylifeunfolding@4146c987a9a6>
2026-03-27 03:38:10 +0000

    update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET KEY', ''),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
