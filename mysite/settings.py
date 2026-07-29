bf485afcd528bcf6ac0c8ccf98ca801611d8fac4
goode8 <drgood@umich.edu>
2026-07-29 09:53:06 -0700

    add favicon, fix static load

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.environ.get('STATIC_ROOT', BASE_DIR / 'staticfiles')
 
 MEDIA_URL = '/media/'
